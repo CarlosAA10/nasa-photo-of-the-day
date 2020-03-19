03b2eb185db58a88169745d77c7578e0f0cc220e
Carlos Alvarado <Carlosalvaradoarguello10@gmail.com>
2020-03-19 00:01:14 -0500

    set up my components and got the right data to render, now all is left is setting up my components the way i want them

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
+import NasaCard from "./Components/NasaCard"; 
 import "./App.css";
 
 function App() {
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <NasaCard/> 
     </div>
+  
   );
 }
 
 export default App;
+
+
+// api_key is: 8hMpW0PNEK4kI7K3dxZPr5J4PbohcBaXIUxZBHa8
---
+++ src/Components/NasaCardInfo.js
@@ -0,0 +1,11 @@
+import React from "react"; 
+const NasaCardInfo = props => {
+    return (
+        <div className="planet-card">
+            <img className="planetary-image" alt="view of the earth from far away" src=""/> 
+            <h2>picture of the planet</h2>
+        </div>
+    ); 
+}; 
+
+export default NasaCardInfo; 
